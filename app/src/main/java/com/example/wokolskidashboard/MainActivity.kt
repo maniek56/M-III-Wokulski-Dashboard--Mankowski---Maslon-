86297984a85a235c764f651d00cfed0f718a75a1
igor <igor.mankowski@cyfroweszkoly.pl>
2026-04-09 15:59:31 +0200

    Prepare MainActivity.kt and MainScreen.kt to work on them

--- app/src/main/java/com/example/wokolskidashboard/MainActivity.kt
+++ app/src/main/java/com/example/wokolskidashboard/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.wokolskidashboard.ui.MainScreen
 import com.example.wokolskidashboard.ui.theme.WokolskiDashBoardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             WokolskiDashBoardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WokolskiDashBoardTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,2 +1,10 @@
 package com.example.wokolskidashboard.ui
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    Text("To jest tekst",
+        modifier = modifier)
+}
