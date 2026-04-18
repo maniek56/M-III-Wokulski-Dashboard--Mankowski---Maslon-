bb3ae2711b44e598cebb2cfff7425c295382753d
Igor Mankowski <igor.mankowski@cyfroweszkoly.pl>
2026-04-18 20:45:13 +0200

    Create data class in Transaction.kt and variable "transactions" in MainScreen.kt

--- app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
+++ app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
@@ -1,2 +1,7 @@
 package com.example.wokolskidashboard.model
 
+data class Transaction(
+    val tittle: String,
+    val amount: Double,
+    val isExpensive: Boolean
+)
--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,10 +1,13 @@
 package com.example.wokolskidashboard.ui
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.example.wokolskidashboard.model.Transaction
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Text("To jest tekst",
-        modifier = modifier)
+    val transactions = remember { mutableStateListOf<Transaction>()}
 }
