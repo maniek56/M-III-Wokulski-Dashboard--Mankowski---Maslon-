34cc69ac20c9143c5f41f4f42208a9c4378d8908
Igor Mankowski <igor.mankowski@cyfroweszkoly.pl>
2026-04-19 16:33:47 +0200

    Create new composable fun IncomeForm in IncomeForm.kt, add name field and amount field

--- app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/IncomeForm.kt
@@ -1,2 +1,18 @@
 package com.example.wokolskidashboard.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.wokolskidashboard.model.Transaction
+
+@Composable
+fun IncomeForm(onAddTransaction: (Transaction) -> Unit ) {
+    var name by remember{mutableStateOf("")}
+
+    var amount by remember{mutableStateOf("")}
+
+}
